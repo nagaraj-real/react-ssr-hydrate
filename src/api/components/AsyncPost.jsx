fe5ec6cfb6b785d30927b8cd00438e136c6f29a9
nagaraj <raj.nagaraj1990@gmail.com>
2023-05-06 01:50:07 -0400

    Fix for loop

--- src/api/components/AsyncPost.jsx
+++ src/api/components/AsyncPost.jsx
@@ -2,10 +2,9 @@ import React from "react"
 import { Post } from "./Post"
 
 export const AsyncPost = async () => {
-    const id = Math.floor(Math.random() * 200);
     let result, data, error;
     try {
-        result = await fetch(`https://jsonplaceholder.typicode.com/todos/${id}/?_delay=5000`);
+        result = await fetch(`https://jsonplaceholder.typicode.com/todos/10/?_delay=5000`);
         data = await result.json();
     } catch (ex) {
         error = ex
